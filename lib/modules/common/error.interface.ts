ee5c973e6a691b288bfc0304d6af15b1f47ed55b
Eric <1945320167@qq.com>
2018-01-17 11:09:25 +0800

    Commit  generic configuration information.

---
+++ lib/modules/common/error.interface.ts
@@ -0,0 +1,7 @@
+import {HttpStatus} from "@nestjs/common";
+export interface IErrorMessages{
+    readonly type:string;
+    readonly httpStatus:HttpStatus;
+    readonly errorMessage:string;
+    readonly userMessage:string;
+}
