135637feef7ec8fd088ca769d2f8d4d7ebb4ee61
Eric <1945320167@qq.com>
2018-01-17 18:46:45 +0800

    add some error message

--- lib/modules/errorMessage/error.config.ts
+++ lib/modules/errorMessage/error.config.ts
@@ -8,4 +8,16 @@ export const errorMessagesConfig: { [messageCode: string]: IErrorMessages } = {
         errorMessage: 'Unable to create a new navigation with missing information.',
         userMessage: 'Unable to create a new navigation with missing information.',
     },
+    'delete:recycling:idMissing':{
+        type: 'BadRequest',
+        httpStatus: HttpStatus.BAD_REQUEST,
+        errorMessage:'The current id corresponding to the data does not exist.',
+        userMessage:'The current id corresponding to the data does not exist.',
+    },
+    'create:publishedTime:lessThan':{
+        type: 'BadRequest',
+        httpStatus: HttpStatus.BAD_REQUEST,
+        errorMessage:'The published time cannot be less than the current time.',
+        userMessage:'The published time cannot be less than the current time.',
+    }
 };
