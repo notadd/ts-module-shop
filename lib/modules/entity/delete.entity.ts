6cdb8155002cf84668ff5aa4aa9c0e7d18a90cc8
Eric <1945320167@qq.com>
2018-01-17 18:47:33 +0800

    Add delete history table.

--- lib/modules/entity/delete.entity.ts
+++ lib/modules/entity/delete.entity.ts
@@ -1,11 +1,12 @@
-import {Column, Entity, UpdateDateColumn} from "typeorm";
+import {Column, Entity, PrimaryColumn, UpdateDateColumn} from "typeorm";
 
 @Entity('history')
 export class DeleteEntity{
+    @PrimaryColumn() id:number;
     //删除文章Id
-    @Column() id:number;
+    @Column() articleId:number;
     //文章名称
-    @Column({length:120}) name:string;
+    @Column({length:120}) articleName:string;
     //删除时间
     @UpdateDateColumn() deleteAt:Date;
 }
