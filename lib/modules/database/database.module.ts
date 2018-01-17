bcb15a82f104d5c5cf71ab4f9b0da2dccfd133c9
Eric <1945320167@qq.com>
2018-01-17 10:42:52 +0800

    commit database module

---
+++ lib/modules/database/database.module.ts
@@ -0,0 +1,9 @@
+import {Module} from "@nestjs/common";
+import {databaseProvider} from "./database.provider";
+
+@Module({
+    components:[...databaseProvider],
+    exports:[...databaseProvider]
+})
+
+export class DatabaseModule{}
