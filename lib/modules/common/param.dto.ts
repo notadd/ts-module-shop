2b385275b40c2bf97e8e9cd7fe2f4a7563eee472
Eric <1945320167@qq.com>
2018-01-17 18:49:19 +0800

    Increase the parameter

--- lib/modules/common/param.dto.ts
+++ lib/modules/common/param.dto.ts
@@ -1,5 +1,6 @@
 import {ApiModelProperty} from "@nestjs/swagger";
-import {IsString,IsInt} from 'class-validator';
+import {IsString,IsInt,IsBoolean,IsDate,IsUrl} from 'class-validator';
+import {Url, URL} from "url";
 
 export class GetLimit{
     @ApiModelProperty({type:Number})
@@ -15,4 +16,85 @@ export class KeyWords{
     @ApiModelProperty({type:String})
     @IsString()
     readonly keyWords;
+}
+
+export class DeleteArticleId{
+    @ApiModelProperty({type:[Number]})
+    @IsInt()
+    readonly id;
+
+}
+
+export class CreateArticle{
+    @ApiModelProperty({type:String})
+    @IsString()
+    readonly name;
+
+    @ApiModelProperty({type:String})
+    @IsString()
+    readonly classify;
+
+    @ApiModelProperty({type:String})
+    @IsString()
+    readonly abstractArticle;
+
+    @ApiModelProperty({type:Number})
+    @IsInt()
+    readonly topPlace;
+
+    @ApiModelProperty({type:Boolean})
+    @IsBoolean()
+    readonly hidden;
+
+    @ApiModelProperty({type:String})
+    @IsString()
+    readonly publishedTime;
+
+    @ApiModelProperty({type:String})
+    @IsString()
+    readonly source;
+
+    @ApiModelProperty({type:URL})
+    @IsUrl()
+    readonly sourceUrl;
+
+}
+
+export class UpdateArticle{
+    @ApiModelProperty({type:Number})
+    @IsInt()
+    readonly id;
+
+    @ApiModelProperty({type:String})
+    @IsString()
+    readonly name;
+
+    @ApiModelProperty({type:String})
+    @IsString()
+    readonly classify;
+
+    @ApiModelProperty({type:String})
+    @IsString()
+    readonly abstractArticle;
+
+    @ApiModelProperty({type:Number})
+    @IsInt()
+    readonly topPlace;
+
+    @ApiModelProperty({type:Boolean})
+    @IsBoolean()
+    readonly hidden;
+
+    @ApiModelProperty({type:Date})
+    @IsDate()
+    readonly publishedTime;
+
+    @ApiModelProperty({type:String})
+    @IsString()
+    readonly source;
+
+    @ApiModelProperty({type:String})
+    @IsUrl()
+    readonly sourceUrl;
+
 }
