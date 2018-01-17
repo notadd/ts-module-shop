6d450bffecab47a1342f7bf9c79ac7ca8460ee97
Eric <1945320167@qq.com>
2018-01-17 17:08:26 +0800

    Add pages to get all articles and keyword search articles.

--- lib/modules/article/article.controller.ts
+++ lib/modules/article/article.controller.ts
@@ -1,15 +1,24 @@
-import {Controller, Get, HttpStatus, Post, Response} from "@nestjs/common";
+import {Body, Controller, Get, HttpStatus, Post, Response} from "@nestjs/common";
 import {ApiOperation} from "@nestjs/swagger";
 import {ArticleService} from "./article.service";
+import {GetLimit} from "../common/param.dto";
+import {KeyWords} from "../common/param.dto";
 
 @Controller()
 export class ArticleController{
     constructor(private  readonly articleService:ArticleService){};
 
     @ApiOperation({title:"find All articles"})
-    @Get()
-    public getArticleAll(@Response() res){
+    @Post('findAll')
+    public getArticleAll(@Response() res,@Body() limitNum:GetLimit){
+        const findAll=this.articleService.getArticleAll(limitNum.limitNumber);
+        return res.status(HttpStatus.OK).send(JSON.stringify(findAll));
+    }
 
-        return res.status(HttpStatus.OK).send();
+    @ApiOperation({title:"Keyword search"})
+    @Post('serach')
+    public keywordsSerach(@Response() res,@Body() key:KeyWords){
+        const findAll=this.articleService.serachArticles(key.keyWords,key.limitNumber);
+        return res.status(HttpStatus.OK).send(JSON.stringify(findAll));
     }
 }
