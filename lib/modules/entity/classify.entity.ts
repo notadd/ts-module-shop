7da500c7ea23b142fe8b130599608eb6596faa0a
Eric <1945320167@qq.com>
2018-01-18 17:09:29 +0800

    The classify table increases the parent-child relationship.

--- lib/modules/entity/classify.entity.ts
+++ lib/modules/entity/classify.entity.ts
@@ -10,25 +10,26 @@ export class ClassifyEntity{
     //分类名称
     @Column({nullable:false,length:120}) classifyName:string;
     //分类别名
-    @Column({nullable:false,length:120}) classifyAlias:string;
+    @Column({nullable:false,length:120}) classifyAlias:string
     //内链
     @Column({nullable:true,length:200}) chainUrl:string;
     //描述
-    @Column({length:200}) describe:string;
+    @Column({nullable:true,length:200}) describe:string;
     //颜色
-    @Column({length:40}) color:string;
+    @Column({nullable:true,length:40}) color:string;
     //是否显示下一级分类文章
     @Column() showNext:boolean;
     //父节点
-    @Column() parentId:number;
-    //创建时间
-    @CreateDateColumn() createAt:Date;
-    //修改时间
-    @UpdateDateColumn() updateAt:Date;
+    @Column({nullable:true}) groupId:number;
 
     @OneToMany(type => ClassifyEntity,ClassifyEntity =>ClassifyEntity.parent,{cascadeInsert:true})
     childrens: ClassifyEntity[];
 
     @ManyToOne(type => ClassifyEntity,ClassifyEntity =>ClassifyEntity.childrens,{cascadeInsert:true})
     parent:ClassifyEntity;
+
+    //创建时间
+    @CreateDateColumn() createAt:Date;
+    //修改时间
+    @UpdateDateColumn() updateAt:Date;
 }
