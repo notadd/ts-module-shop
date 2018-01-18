27274abe48071b2fbee15910bec2a12bdd6de9b6
Eric <1945320167@qq.com>
2018-01-18 17:01:26 +0800

    Add an unlimited classification method.

--- lib/modules/classify/classify.service.ts
+++ lib/modules/classify/classify.service.ts
@@ -2,6 +2,7 @@ import {Component, Inject} from "@nestjs/common";
 import {Repository} from "typeorm";
 import {ClassifyEntity} from "../entity/classify.entity";
 import {MessageCodeError} from "../errorMessage/error.interface";
+import {getManager} from "typeorm";
 
 @Component()
 export class ClassifyService{
@@ -13,16 +14,15 @@ export class ClassifyService{
      * @param {string} parent
      * @returns {Promise<ClassifyEntity[]>}
      */
-    async createClassify(entity:ClassifyEntity,parent:string):Promise<ClassifyEntity[]>{
+    async createClassify(entity:ClassifyEntity):Promise<ClassifyEntity[]>{
         let newClassify:ClassifyEntity[] = await this.repository.createQueryBuilder().where('"classifyAlias"= :classifyAlias',{classifyAlias:entity.classifyAlias}).getMany();
         //别名不能重复
         if(newClassify.length>0) throw new MessageCodeError('create:classify:aliasRepeat');
-        let parentClassify:ClassifyEntity = await this.repository.createQueryBuilder().where('"classifyAlias"= :classifyAlias',{classifyAlias:parent}).getOne();
+        let parentClassify:ClassifyEntity = await this.repository.findOneById(entity.groupId);
         //通过父级别名确定父级是否存在
         if(parentClassify==null) throw new MessageCodeError('create:classify:parentIdMissing');
-        entity.parentId = parentClassify.id;
         this.repository.insert(entity);
-       return this.findAllClassify(1);
+       return this.findAllClassify();
     }
 
     /**
@@ -37,14 +37,13 @@ export class ClassifyService{
         let newClassify:ClassifyEntity[] = await this.repository.createQueryBuilder().where('"classifyAlias"= :classifyAlias',{classifyAlias:entity.classifyAlias}).getMany();
         //别名不能重复
         if(newClassify.length>0) throw new MessageCodeError('create:classify:aliasRepeat');
-        let parentClassify:ClassifyEntity = await this.repository.createQueryBuilder().where('"classifyAlias"= :classifyAlias',{classifyAlias:parent}).getOne();
+        let parentClassify:ClassifyEntity = await this.repository.findOneById(entity.groupId);
         //通过父级别名确定父级是否存在
         if(parentClassify==null) throw new MessageCodeError('create:classify:parentIdMissing');
-        entity.parentId = parentClassify.id;
         entity.updateAt =new Date();
         let finalClassify:ClassifyEntity =entity;
         await this.repository.updateById(entity.id,finalClassify);
-        return this.findAllClassify(1);
+        return this.findAllClassify();
      }
 
     /**
@@ -52,9 +51,67 @@ export class ClassifyService{
      * @param {number} id
      * @returns {Promise<ClassifyEntity[]>}
      */
-    async findAllClassify(id:number):Promise<ClassifyEntity[]>{
+    async findAllClassify():Promise<ClassifyEntity[]>{
+        await getManager().query("update public.classify set \"parentId\" = \"groupId\"");
         const classify=await this.repository.createQueryBuilder('classify').innerJoinAndSelect('classify.childrens','childrens').orderBy('classify.id').getMany();
-        console.log('classify='+JSON.stringify(classify));
+       // console.log('classify='+JSON.stringify(classify));
+        let finalArray:ClassifyEntity[]=await this.recursion(this.deletegroup(0,classify));
+        console.log('finalArray='+JSON.stringify(finalArray));
+        return classify;
+    }
+
+    /**
+     * 无极限分类
+     * @param {ClassifyEntity[]} entity
+     * @returns {Promise<ClassifyEntity[]>}
+     */
+    async recursion(entity:ClassifyEntity[]):Promise<ClassifyEntity[]>{
+        let returnArray:ClassifyEntity[]=[];
+        for(let t in entity){
+            let firstArray:ClassifyEntity[] = entity[t].childrens;
+            for(let h in firstArray){
+                let num:number =firstArray[h].id;
+                for(let m in entity){
+                    if(num == entity[m].id){
+                        firstArray[h].childrens = entity[m].childrens;
+                        entity[t].childrens=firstArray;
+                        returnArray.push(entity[t]);
+                       // console.log('deletegroup='+JSON.stringify(this.deletegroup(num,entity)));
+                        /*this.recursion(*//*this.deletegroup(num,entity)*//*);*/
+                    }
+                }
+            }
+
+        }
+        //console.log('returnArray='+JSON.stringify(returnArray));
+        return returnArray;
+    }
+
+    /**
+     * 数组删除数据
+     */
+    deletegroup(id:number,entity:ClassifyEntity[]){
+        let array:ClassifyEntity[]=[];
+        for(let t in entity){
+            if(entity[t].id!=id){
+                console.log('删除数据'+id);
+                array.push(entity[t]);
+            }
+        }
+        return array;
+    }
+
+
+    /**
+     * 通过Id删除分类及对应的子分类
+     * @param {number} id
+     * @returns {Promise<ClassifyEntity[]>}
+     */
+    async deleteClassify(id:number):Promise<ClassifyEntity[]>{
+        let classify:ClassifyEntity = await this.repository.findOneById(id);
+        if(classify==null) throw new MessageCodeError('update:classify:updateById');
+        await getManager().query("update public.classify set \"parentId\" = \"groupId\"");
+        const result =await this.repository.createQueryBuilder('classify').innerJoinAndSelect('classify.childrens','childrens').orderBy('classify.id').getMany();
         return;
     }
 }
