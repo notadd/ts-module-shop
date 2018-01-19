24b6b748a1cf9e850f0a1415d48d9118b9eb087b
Eric <1945320167@qq.com>
2018-01-19 10:36:05 +0800

    Add information item management entities.

---
+++ lib/modules/entity/information.entity.ts
@@ -0,0 +1,20 @@
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+import {stringify} from "querystring";
+
+@Entity('information')
+export class InformationEntity{
+    //信息项Id
+    @PrimaryGeneratedColumn() id:number;
+    //信息项名称
+    @Column({nullable:false,length:200}) informationName:string;
+    //信息项介绍
+    @Column({nullable:true,length:200}) introduce:string;
+    //信息项类型
+    @Column({length:100}) type:string;
+    //是否必填
+    @Column() mandatory:boolean;
+    //排序
+    @Column({nullable:true}) sort:number
+
+
+}
