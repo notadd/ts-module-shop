14f703e0535696622c265da80394ed3c2822434c
Eric <1945320167@qq.com>
2018-01-19 17:19:11 +0800

    Add functionality to page categorization.

--- lib/modules/classify/classify.module.ts
+++ lib/modules/classify/classify.module.ts
@@ -1,13 +1,15 @@
 import {Module} from "@nestjs/common";
 import {DatabaseModule} from "../database/database.module";
 import {ClassifyController} from "./classify.controller";
-import {respositoryProvider} from "./respository.provider";
+import {clarespositoryProvider} from "./respository.provider";
 import {ClassifyService} from "./classify.service";
+import {respositoryProvider} from "../article/respository.provider";
+import {pageClassifyProvider} from "./respository.provider";
 
 @Module({
     modules:[DatabaseModule],
     controllers:[ClassifyController],
-    components:[...respositoryProvider,ClassifyService],
+    components:[...clarespositoryProvider,...respositoryProvider,...pageClassifyProvider,ClassifyService],
     exports:[ClassifyService]
 })
 
