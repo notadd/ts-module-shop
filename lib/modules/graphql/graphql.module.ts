0a6ca752705eb8ca44bf10f148aa48b0df1a8f2c
Eric <1945320167@qq.com>
2018-01-22 11:34:42 +0800

    添加graphql.module

---
+++ lib/modules/graphql/graphql.module.ts
@@ -0,0 +1,11 @@
+import {Module} from "@nestjs/common";
+import {GraphqlResolver} from "./graphql.resolver";
+import {ArticleModule} from "../article/article.module";
+import {PageModule} from "../page/page.module";
+import {ClassifyModule} from "../classify/classify.module";
+
+@Module({
+    modules:[ArticleModule,PageModule,ClassifyModule],
+    components:[GraphqlResolver],
+})
+export class GraphqlModule{}
