8ee842536f1e8844632e6530a2d795179d3ff1be
Eric <1945320167@qq.com>
2018-01-22 11:35:38 +0800

    在graphql中添加根据关键字搜索的方法

--- lib/modules/graphql/graphql.resolver.ts
+++ lib/modules/graphql/graphql.resolver.ts
@@ -12,7 +12,7 @@ export class GraphqlResolver{
      * @returns {Promise<ArticleEntity[]>}
      */
     @Query()
-    findAllArticles(obj,arg){
+    getArticleAll(obj,arg){
         const str:string=JSON.stringify(arg);
         let bToJSon=JSON.parse(str);
         let map =new Map();
@@ -21,6 +21,24 @@ export class GraphqlResolver{
         const result=this.articleService.getArticleAll(limit);
         return result;
 
+    }
+
+    /**
+     * 分页根据关键字搜索
+     * @param obj
+     * @param arg
+     * @returns {Promise<ArticleEntity[]>}
+     */
+    @Query()
+    keywordsSerach(obj,arg){
+        const str:string=JSON.stringify(arg);
+        let bToJSon=JSON.parse(str);
+        let map =new Map();
+        map=this.objToStrMap(bToJSon);
+        let limit:number=map.get('limitNum');
+        let keyWords:string=map.get('keywords');
+        const result=this.articleService.serachArticles(keyWords,limit);
+        return result;
     }
     /**
      * JSON----Map
