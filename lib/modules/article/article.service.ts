baf25e78770c7f819d67445bf5aa716ddd3eac56
Eric <1945320167@qq.com>
2018-01-23 12:28:09 +0800

    update some methods

--- lib/modules/article/article.service.ts
+++ lib/modules/article/article.service.ts
@@ -1,18 +1,24 @@
 import {Component, Inject} from "@nestjs/common";
-import {getManager, Repository} from "typeorm";
+import { Repository} from "typeorm";
 import {ArticleEntity} from "../entity/article.entity";
+import {MessageCodeError} from "../errorMessage/error.interface";
+import {HistoryEntity} from "../entity/history.entity";
+import {HistoryService} from "../history/history.service";
 
 @Component()
 export class ArticleService{
-constructor(@Inject('ArticleRepositoryToken') private readonly respository:Repository<ArticleEntity>){}
+constructor(@Inject('ArticleRepositoryToken') private readonly respository:Repository<ArticleEntity>,
+            private readonly historyService:HistoryService){}
 
     /**
      * 返回所有数据,依据提供limit number 进行分页
      * @returns {Promise<ArticleEntity[]>}
      */
     async  getArticleAll(limit:number):Promise<ArticleEntity[]>{
-        let resultAll:ArticleEntity[]= await this.respository.createQueryBuilder().orderBy('article.id',"ASC").limit(limit).getMany();
-     return resultAll;
+        console.log('limitNum='+limit);
+        let resultAll:ArticleEntity[]= await this.respository.createQueryBuilder().orderBy('id',"ASC").limit(limit).getMany();
+        console.log('resultFind='+JSON.stringify(resultAll));
+        return resultAll;
     }
 
     /**
@@ -24,7 +30,103 @@ constructor(@Inject('ArticleRepositoryToken') private readonly respository:Repos
     async serachArticles(name:string,limit:number):Promise<ArticleEntity[]>{
         let str:string=`%${name}%`;
         console.log('字符串拼接=='+str);
-        let resultAll:ArticleEntity[]=await this.respository.createQueryBuilder().where('"name"like :name',{name:str,}).limit(limit).orderBy('article.id','ASC').getMany();
+        let resultAll:ArticleEntity[]=await this.respository.createQueryBuilder().where('"name"like :name',{name:str,}).orderBy('id','ASC').limit(limit).getMany();
         return resultAll;
+    }
+
+    /**
+     * 修改数据状态为回收站
+     * @param {[number]} array
+     * @returns {Promise<number>}
+     */
+     async deleteArticles(array:[number]):Promise<number>{
+        let count:number=0;
+        for(let t in array){
+            let article:ArticleEntity=await this.respository.findOneById(array[t]);
+            if(article=null) throw new MessageCodeError('delete:recycling:idMissing');
+            article.recycling=true;
+            article.updateAt =new Date();
+            let newArticle:ArticleEntity=article;
+            this.respository.updateById(array[t],newArticle);
+            count++;
+        }
+        return count;
+      }
+
+    /**
+     * 添加文章
+     * @param {ArticleEntity} article
+     * @returns {Promise<void>}
+     */
+      async createArticle(article:ArticleEntity):Promise<ArticleEntity[]>{
+          if(article.publishedTime<new Date()) throw new MessageCodeError('create:publishedTime:lessThan');
+          if(article.classifyId==0 ||article.classifyId==null)
+              article.classifyId=null;
+          console.log('输入时间='+article.publishedTime);
+          this.respository.insert(article);
+         return this.getArticleAll(0);
+      }
+
+    /**
+     * 修改文章
+     * @param {ArticleEntity} article
+     * @returns {Promise<void>}
+     */
+      async updateArticle(article:ArticleEntity):Promise<ArticleEntity[]>{
+          let art:ArticleEntity =await this.respository.findOneById(article.id);
+          if(art==null) throw new MessageCodeError('delete:recycling:idMissing');
+          art.updateAt=new Date();
+          let newArt:ArticleEntity =art;
+          this.respository.updateById(newArt.id,newArt);
+          return this.getArticleAll(0);
+      }
+
+    /**
+     * 分页获取回收站内所有文章
+     * @param {number} limit
+     * @returns {Promise<ArticleEntity[]>}
+     */
+      async recycleFind(limit:number):Promise<ArticleEntity[]>{
+          let result:ArticleEntity[]=await this.respository.createQueryBuilder().where('"recycling"= :recycling',{recycling:true}).limit(limit).getMany();
+          return result;
+      }
+
+    /**
+     * 回收站内删除数据，记入历史表
+     * @param {[number]} array
+     * @returns {Promise<number>}
+     */
+    async recycleDelete(array:[number]):Promise<number>{
+        let count:number=0;
+        let history:HistoryEntity[] =[];
+        for(let t in array){
+            let article:ArticleEntity=await this.respository.findOneById(array[t]);
+            if(article=null) throw new MessageCodeError('delete:recycling:idMissing');
+            let his:HistoryEntity =new HistoryEntity();
+            his.articleId =article.id;
+            his.articleName =article.name;
+            this.respository.deleteById(article.id);
+            count++;
+            history.push(his);
+        }
+        this.historyService.createHistory(history);
+        return count;
+    }
+
+    /**
+     * 回收站内批量或者单个还原数据，目前限制分页为10
+     * @param {[number]} array
+     * @returns {Promise<ArticleEntity[]>}
+     */
+    async reductionArticle(array:[number]):Promise<ArticleEntity[]>{
+        for(let t in array){
+            let article:ArticleEntity=await this.respository.findOneById(array[t]);
+            if(article=null) throw new MessageCodeError('delete:recycling:idMissing');
+            article.recycling=false;
+            article.updateAt =new Date();
+            let newArticle:ArticleEntity=article;
+            this.respository.updateById(array[t],newArticle);
+        }
+        return this.recycleFind(10);
     }
 }
