06ae7a6e41381ba958f6c948676aa9b4e8fb9095
Eric <1945320167@qq.com>
2018-01-23 17:05:10 +0800

    update the useFactory

--- lib/modules/classify/respository.provider.ts
+++ lib/modules/classify/respository.provider.ts
@@ -1,10 +1,17 @@
 import {Connection} from "typeorm";
 import {ClassifyEntity} from "../entity/classify.entity";
+import {PageClassifyEntity} from "../entity/pageClassify.entity";
 
-export const respositoryProvider=[
+export const clarespositoryProvider=[
     {
-        provide:"ArticleRepositoryToken",
+        provide:"ClassifyRepositoryToken",
         useFactory:(connection:Connection) =>connection.getRepository(ClassifyEntity),
         inject:['DbConnectionToken']
     }
 ]
+
+export const pageClassifyProvider=[{
+    provide:"PageClassifyRepositoryToken",
+    useFactory:(connection:Connection) =>connection.getRepository(PageClassifyEntity),
+    inject:['DbConnectionToken']
+}]
