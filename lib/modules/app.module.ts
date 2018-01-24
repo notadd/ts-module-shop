671c55c0befd4f6e904ddf39a5e47e38c46f9434
Eric <1945320167@qq.com>
2018-01-24 16:50:42 +0800

    add the function to create schema

--- lib/modules/app.module.ts
+++ lib/modules/app.module.ts
@@ -3,10 +3,14 @@ import {MiddlewaresConsumer} from "@nestjs/common";
 import {graphqlExpress,graphiqlExpress} from "apollo-server-express";
 import {RequestMethod} from "@nestjs/common";
 import {GraphQLFactory,GraphQLModule} from "@nestjs/graphql";
-
+import {ArticleModule} from "./article/article.module";
+import {ClassifyModule} from "./classify/classify.module";
+import {HistoryModule} from "./history/history.module";
+import {PageModule} from "./page/page.module";
+import {GraphqlModule} from "./graphql/graphql.module";
 
 @Module({
-    modules :[GraphQLModule],
+    modules :[GraphQLModule,ArticleModule,ClassifyModule,GraphqlModule,PageModule],
 })
 
 export class ApplicationModule implements NestModule{
@@ -14,14 +18,15 @@ export class ApplicationModule implements NestModule{
     //中间件设置
     configure(consumer:MiddlewaresConsumer){
         const schema =this.createSchema();
+       // console.log('schema='+JSON.stringify(schema));
         consumer.apply(graphiqlExpress({ endpointURL: '/graphql' }))
             .forRoutes({ path: '/graphiql', method: RequestMethod.GET })
             .apply(graphqlExpress(req => ({ schema, rootValue: req })))
             .forRoutes({ path: '/graphql', method: RequestMethod.ALL });
     }
-    createSchema() {
-        const typeDefs = this.graphqlFactory.mergeTypesByPaths('./**/*.graphql');
-        const schema = this.graphqlFactory.createSchema({ typeDefs });
-        return schema;
-    }
+     createSchema() {
+         const typeDefs = this.graphqlFactory.mergeTypesByPaths('./**/*.graphql');
+         const schema = this.graphqlFactory.createSchema({ typeDefs });
+         return schema;
+     }
 }
