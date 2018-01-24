deea525e6217770f4efcfafeec4f3039be375e3d
Eric <1945320167@qq.com>
2018-01-24 16:49:57 +0800

    add historyModule,ClassifyModule

--- lib/modules/page/page.module.ts
+++ lib/modules/page/page.module.ts
@@ -3,11 +3,13 @@ import {DatabaseModule} from "../database/database.module";
 import {PageController} from "./page.controller";
 import {PageService} from "./page.service";
 import {pagerespositoryProvider} from "./repository.provider";
+import {HistoryModule} from "../history/history.module";
+import {ClassifyModule} from "../classify/classify.module";
 
 @Module({
-    modules:[DatabaseModule],
+    modules:[DatabaseModule,HistoryModule,ClassifyModule],
     controllers:[PageController],
-    components:[PageService,...pagerespositoryProvider],
+    components:[...pagerespositoryProvider,PageService],
     exports:[PageService]
 })
 export class PageModule{}
