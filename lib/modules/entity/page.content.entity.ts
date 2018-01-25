974dd781ae6da354434279b4ccb307b2ea8f9aa2
Eric <1945320167@qq.com>
2018-01-25 14:08:34 +0800

    create page_content_table

---
+++ lib/modules/entity/page.content.entity.ts
@@ -0,0 +1,19 @@
+import {Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
+import {PageEntity} from "./page.entity";
+
+@Entity('page_content_table')
+export class PageContentEntity{
+    //内容Id
+    @PrimaryGeneratedColumn() id:number;
+    //页面Id
+    @Column() parentId:number;
+    //页面内容
+    @Column({nullable:true,length:10000}) content:string;
+    //创建时间
+    @CreateDateColumn() createAt:Date;
+    //修改时间
+    @UpdateDateColumn() updateAt:Date;
+
+    @ManyToOne(type => PageEntity,PageEntity=>PageEntity.contents)
+    pages:PageEntity;
+}
