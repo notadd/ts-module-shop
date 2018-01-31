c03d5e63516ae31686647aa3bdf42ec4d3fe7765
Eric <1945320167@qq.com>
2018-01-31 19:07:40 +0800

    change some methods

--- lib/modules/page/page.module.ts
+++ lib/modules/page/page.module.ts
@@ -6,11 +6,12 @@ import {pagerespositoryProvider} from "./repository.provider";
 import {contentrespositoryProvider} from "./repository.provider";
 import {HistoryModule} from "../history/history.module";
 import {ClassifyModule} from "../classify/classify.module";
+import {pageClassifyProvider} from "../classify/respository.provider";
 
 @Module({
     modules:[DatabaseModule,HistoryModule,ClassifyModule],
     controllers:[PageController],
-    components:[...pagerespositoryProvider,...contentrespositoryProvider,PageService],
+    components:[...pagerespositoryProvider,...contentrespositoryProvider,...pageClassifyProvider,PageService],
     exports:[PageService]
 })
 export class PageModule{}
