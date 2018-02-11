957d0c4b124402870c61c1d00a9b6969a2a81b35
Eric <1945320167@qq.com>
2018-02-11 18:48:44 +0800

    commit delete-param.command.ts file

---
+++ lib/modules/siteMapXml/commands/impl/delete-param.command.ts
@@ -0,0 +1,21 @@
+import {ICommand} from "@nestjs/cqrs";
+
+export class DeleteParamCommand implements ICommand {
+    constructor(public readonly heroId: string, public readonly itemId: string) {}
+    /*constructor( lc_is_Enabled_XML_Sitemap:boolean,
+
+                 //xml文件名
+                 lc_XML_FileName:boolean,
+
+                 //只包括最近的文章(1000以内)
+                 lc_post_limit1000:boolean,
+
+                 //当发布文章时更新sitemap
+                 lc_is_update_sitemap_when_post:boolean,
+
+                 //链接包括：文章
+                 lc_post_select:boolean,
+
+                 //链接包括：页面
+                 lc_page_select:boolean){}*/
+}
---
+++ lib/modules/siteMapXml/events/handlers/index.ts
@@ -0,0 +1,4 @@
+import {PageDeleteHandler} from "./page-delete.handler";
+import {PageCreateHandler} from "./page-create.handler";
+
+export const EventHandlers=[PageDeleteHandler,PageCreateHandler];
