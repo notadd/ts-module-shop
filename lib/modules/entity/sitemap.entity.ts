9b5236d6c9209b7dcb19a8d56cda005a4910fa4a
Eric <1945320167@qq.com>
2018-02-11 18:36:54 +0800

    add a sitemap entities

---
+++ lib/modules/entity/sitemap.entity.ts
@@ -0,0 +1,27 @@
+import {Column, CreateDateColumn, Entity, PrimaryGeneratedColumn} from "typeorm";
+
+@Entity('sitemap_entity_table')
+export class SitemapEntity{
+    //基本配置Id
+    @PrimaryGeneratedColumn() id:number;
+    //文件名
+    @Column({length:20})
+    lc_XML_FileName:string;
+    //生成xml地图
+    @Column()
+    lc_is_Enabled_Html_Sitemap:boolean;
+    //只更新最近文章(1000以内)
+    @Column()
+    lc_post_limit1000:boolean;
+    //链接包括：文章
+    @Column()
+    lc_post_select:boolean;
+    //链接包括：页面
+    @Column()
+    lc_page_select:boolean;
+    //当发布文章时更新
+    @Column()
+    lc_is_update_sitemap_when_post:boolean;
+    //创建时间
+    @CreateDateColumn() createAt:Date;
+}
