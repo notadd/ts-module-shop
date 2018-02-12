378b74958a8a352dc033070b0fc256d1e8ee1953
Eric <1945320167@qq.com>
2018-02-12 19:13:05 +0800

    add page param command

---
+++ lib/modules/siteMapXml/commands/impl/page-param.command.ts
@@ -0,0 +1,3 @@
+export class PageParamCommand{
+    constructor(public  id:number){}
+}
--- lib/modules/siteMapXml/events/handlers/index.ts
+++ lib/modules/siteMapXml/events/handlers/index.ts
@@ -1,4 +1,7 @@
-import {PageDeleteHandler} from "./page-delete.handler";
-import {PageCreateHandler} from "./page-create.handler";
+/*import {PageDeleteHandler} from "./page-delete.handler";
+import {PageCreateHandler} from "./page-create.handler";*/
+import {SitemapCreateHandler} from "./sitemap-create.handler";
+import {SitemapUpdateHandler} from "./sitemap-update.handler";
+import {PageCurdHandle} from "./page-curd.handle";
 
-export const EventHandlers=[PageDeleteHandler,PageCreateHandler];
+export const EventHandlers=[/*PageDeleteHandler,PageCreateHandler,*/SitemapCreateHandler,SitemapUpdateHandler,PageCurdHandle];
