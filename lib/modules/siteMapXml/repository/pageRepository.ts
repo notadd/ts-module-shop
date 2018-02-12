d0174a2432db49fca7bf97be0ae553c3a82b53bc
Eric <1945320167@qq.com>
2018-02-12 19:18:56 +0800

    commit some methods

--- lib/modules/siteMapXml/repository/pageRepository.ts
+++ lib/modules/siteMapXml/repository/pageRepository.ts
@@ -1,7 +1,8 @@
 import {Page} from "../models/page.model";
-import {userPage} from "./fixtures/page";
+import {siteMap, userPage} from "./fixtures/page";
 import {Component} from "@nestjs/common";
 import {SitemapService} from "../../sitemap/sitemap.service";
+import {Sitemap} from "../models/sitemap.model";
 
 @Component()
 export class PageRepository{
@@ -9,16 +10,7 @@ export class PageRepository{
     async find(id:string): Promise<Page>{
         return userPage ;
     }
-    async createXml(CreateParamCommand){
-        let array_baidu_sitemap_options ={
-            lc_XML_FileName:CreateParamCommand.lc_XML_FileName,
-            lc_is_Enabled_XML_Sitemap:CreateParamCommand.lc_is_Enabled_XML_Sitemap,
-            lc_is_update_sitemap_when_post:CreateParamCommand.lc_is_update_sitemap_when_post,
-            lc_post_limit1000:CreateParamCommand.lc_post_limit1000,
-            lc_page_select:CreateParamCommand.lc_page_select,
-            lc_post_select:CreateParamCommand.lc_post_select};
-        let url:string='localhost:3001/graphiql';
-        this.sitemapService.commitXML(array_baidu_sitemap_options,url);
+    async siteMap():Promise<Sitemap>{
+        return siteMap;
     }
-
 }
