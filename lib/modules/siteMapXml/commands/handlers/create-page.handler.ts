a154ea367a4e1cdd188b4a1a05aa97cd37b1ea9f
Eric <1945320167@qq.com>
2018-02-24 16:39:14 +0800

    Add the return value to the create new page.

--- lib/modules/siteMapXml/commands/handlers/create-page.handler.ts
+++ lib/modules/siteMapXml/commands/handlers/create-page.handler.ts
@@ -2,19 +2,24 @@ import {CommandHandler, EventPublisher, ICommandHandler} from "@nestjs/cqrs";
 import {CreateParamCommand} from "../impl/create-param.command";
 import {PageRepository} from "../../repository/pageRepository";
 import {PageParamCommand} from "../impl/page-param.command";
+import {PageEntity} from "../../../entity/page.entity";
+import {HttpException} from "@nestjs/common";
+import {PageService} from "../../../page/page.service";
 
 const clc=require('cli-color');
 @CommandHandler(PageParamCommand)
 export class CreatePageHandler implements ICommandHandler<PageParamCommand>{
     constructor(private readonly repositoty:PageRepository,
-                private readonly publisher:EventPublisher){}
+                private readonly publisher:EventPublisher,
+                readonly pageService: PageService){}
 
-                async execute(command:PageParamCommand,resolver:(value?) => void){
+                async execute(command:PageParamCommand,resolver:(value/*:PageEntity | HttpException*/) => void):Promise<any>{
                 console.log(clc.greenBright('handlerCommand  PageFindByIdCommand...'));
-               // const id=command;
                 let id:string='0';
                 const page=this.publisher.mergeObjectContext( await this.repositoty.find(id));
-                const result=page.findOneById(command.id);
+                page.createPage({page:command.page,content:command.content,limit:command.limit,pages:command.pages,array:command.array});
+                console.log('command='+JSON.stringify(command));
+                let result=await this.pageService.getAllPage(command.limit,command.pages);
                 console.log('result='+JSON.stringify(result));
                 page.commit();
                 resolver(result);
--- lib/modules/siteMapXml/commands/handlers/create-sitemap.handler.ts
+++ lib/modules/siteMapXml/commands/handlers/create-sitemap.handler.ts
@@ -1,7 +1,6 @@
 import {CommandHandler, EventPublisher, ICommandHandler} from "@nestjs/cqrs";
 import {CreateParamCommand} from "../impl/create-param.command";
 import {PageRepository} from "../../repository/pageRepository";
-import {SitemapResolver} from "../../sitemap.controller";
 
 const clc=require('cli-color');
 @CommandHandler(CreateParamCommand)
@@ -11,7 +10,6 @@ export class CreateSitemapHandler implements ICommandHandler<CreateParamCommand>
 
     async execute(command:CreateParamCommand,resolver:(value?) => void){
         console.log(clc.greenBright('handlerCommand  CreateXmlCommand...'));
-        // const id=command;
         console.log('command='+JSON.stringify(command));
         const sitemap=this.publisher.mergeObjectContext( await this.repositoty.siteMap());
         sitemap.createxml(command);
