30f6d212b5cc866b279b428d93046c4496eb79f8
Eric <1945320167@qq.com>
2018-02-24 17:55:25 +0800

    add a param command

--- lib/modules/siteMapXml/commands/handlers/index.ts
+++ lib/modules/siteMapXml/commands/handlers/index.ts
@@ -3,5 +3,6 @@ import {CreatePageHandler} from "./create-page.handler";*/
 import {CreateSitemapHandler} from "./create-sitemap.handler";
 import {UpdateSitemapHandler} from "./update-sitemap.handler";
 import {CreatePageHandler} from "./create-page.handler";
+import {GetPageHandler} from "./get-page.handler";
 
-export const CommandHandlers = [CreatePageHandler,CreateSitemapHandler,UpdateSitemapHandler];
+export const CommandHandlers = [CreatePageHandler,CreateSitemapHandler,UpdateSitemapHandler,GetPageHandler];
---
+++ lib/modules/siteMapXml/commands/impl/classify-param.command.ts
@@ -0,0 +1,5 @@
+import {ClassifyCurdVm} from "../../models/view/classify-curd.vm";
+
+export class ClassifyParamCommand{
+    constructor(public classify:ClassifyCurdVm){}
+}
