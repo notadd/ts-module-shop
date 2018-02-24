01b99932d3d6b25b2d27802b643566bfe8d610c2
Eric <1945320167@qq.com>
2018-02-24 17:32:40 +0800

    add a new param file

--- lib/modules/siteMapXml/models/view/create-page.vm.ts
+++ lib/modules/siteMapXml/models/view/create-page.vm.ts
@@ -2,15 +2,6 @@ import {PageEntity} from "../../../entity/page.entity";
 import {PageContentEntity} from "../../../entity/page.content.entity";
 
 export class CreatePageVm{
-   /* public readonly  title:string;
-    //页面别名
-    public readonly  alias:string;
-    //页面分类Id
-    public readonly  classifyId:number;
-    //页面分类Id
-    public readonly  classify:string;
-    //页面内容
-    public readonly  contents:[string]*/
     public  page?:PageEntity;
     public content?:PageContentEntity[];
     public limit:number;
