8add0b1f686cdb5e1c71a0f583bcd7b86a4bb28a
Eric <1945320167@qq.com>
2018-02-24 17:33:13 +0800

    add a new handler layer

---
+++ lib/modules/siteMapXml/commands/handlers/get-page.handler.ts
@@ -0,0 +1,38 @@
+import {CommandHandler, EventPublisher, ICommandHandler} from "@nestjs/cqrs";
+import {CreateParamCommand} from "../impl/create-param.command";
+import {PageRepository} from "../../repository/pageRepository";
+import {PageParamCommand} from "../impl/page-param.command";
+import {PageEntity} from "../../../entity/page.entity";
+import {HttpException} from "@nestjs/common";
+import {PageService} from "../../../page/page.service";
+import {GetPageParamCommand} from "../impl/get-page-param.command";
+
+const clc=require('cli-color');
+@CommandHandler(GetPageParamCommand)
+export class GetPageHandler implements ICommandHandler<GetPageParamCommand>{
+    constructor(private readonly repositoty:PageRepository,
+                private readonly publisher:EventPublisher,
+                readonly pageService: PageService){}
+
+    async execute(command:GetPageParamCommand,resolver:(value) => void){
+        console.log(clc.greenBright('handlerCommand getPage Command...'));
+        let result;
+        //关键字搜索
+        if(command.keywords){
+           result=this.pageService.serachKeywords(command.keywords,command.limit,command.pages);
+        }
+        //分类id获取页面
+        if(command.classifyId){
+            result=this.pageService.findPageByClassifyId(command.classifyId,command.limit,command.pages);
+        }
+        //根据页面id获取具体内容
+        if(command.id){
+            result=this.pageService.findPageById(command.id);
+        }
+        //获取所有页面
+        if(command.getAll){
+            result=this.pageService.getAllPage(command.limit,command.pages);
+        }
+        resolver(result);
+    }
+}
