022b28d664aa1750db66747bce23d045c2dd520b
Eric <1945320167@qq.com>
2018-02-26 12:28:46 +0800

    add a new events file

--- lib/modules/siteMapXml/events/handlers/index.ts
+++ lib/modules/siteMapXml/events/handlers/index.ts
@@ -4,5 +4,6 @@ import {SitemapCreateHandler} from "./sitemap-create.handler";
 import {SitemapUpdateHandler} from "./sitemap-update.handler";
 import {PageCurdHandle} from "./page-curd.handler";
 import {ClassifyCurdEvent} from "./classify-curd.handler";
+import {ArticleCurdEvent} from "./article-curd.handler";
 
-export const EventHandlers=[/*PageDeleteHandler,PageCreateHandler,*/SitemapCreateHandler,SitemapUpdateHandler,PageCurdHandle,ClassifyCurdEvent];
+export const EventHandlers=[/*PageDeleteHandler,PageCreateHandler,*/SitemapCreateHandler,SitemapUpdateHandler,PageCurdHandle,ClassifyCurdEvent,ArticleCurdEvent];
