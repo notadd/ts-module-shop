b0b8e5ff9bb26d2d378af724bf750c250242e535
Eric <1945320167@qq.com>
2018-02-27 19:11:57 +0800

    Modify the page control layer return value.

--- lib/modules/siteMapXml/events/impl/page-curd.event.ts
+++ lib/modules/siteMapXml/events/impl/page-curd.event.ts
@@ -1,10 +1,9 @@
 import {PageEntity} from "../../../entity/page.entity";
 import {PageContentEntity} from "../../../entity/page.content.entity";
+import {CreatePageVm} from "../../models/view/create-page.vm";
 
 export class PageCurdEvent{
     constructor(
-        public  page:PageEntity,
-        public content:PageContentEntity[],
-        public  array?:number[]
+        public  pageEntity:CreatePageVm
     ){}
 }
--- lib/modules/siteMapXml/events/impl/sitemap-create.event.ts
+++ lib/modules/siteMapXml/events/impl/sitemap-create.event.ts
@@ -1,22 +1,6 @@
 import {IEvent} from "@nestjs/cqrs";
+import {CreateXmlVm} from "../../models/view/create-xml-vm";
 
 export class SitemapCreateEvent implements IEvent{
-    constructor(
-        public  lc_is_Enabled_XML_Sitemap:boolean,
-
-        //xml文件名
-        public  lc_XML_FileName:boolean,
-
-        //只包括最近的文章(1000以内)
-        public  lc_post_limit1000:boolean,
-
-        //当发布文章时更新sitemap
-        public  lc_is_update_sitemap_when_post:boolean,
-
-        //链接包括：文章
-        public  lc_post_select:boolean,
-
-        //链接包括：页面
-        public  lc_page_select:boolean
-    ){}
+    constructor(public createXml:CreateXmlVm){}
 }
