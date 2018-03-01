45625ecaa7f96fe8a82f184e8084af3cb5b5a740
Eric <1945320167@qq.com>
2018-03-01 14:30:24 +0800

    Delete part of command.

--- lib/modules/cqrsCms/commands/handlers/index.ts
+++ lib/modules/cqrsCms/commands/handlers/index.ts
@@ -1,12 +1,11 @@
-/*import {DeletePageHandler} from "./delete-page.handler";
-import {CreatePageHandler} from "./create-page.handler";*/
 import {CreateSitemapHandler} from "./create-sitemap.handler";
 import {UpdateSitemapHandler} from "./update-sitemap.handler";
 import {CreatePageHandler} from "./page-curd.handler";
 import {GetPageHandler} from "./get-page.handler";
 import {ClassifyCurdHandler} from "./classify-curd.handler";
 import {ArticleCurdHandler} from "./article-curd.handler";
-import {GetArticleHandler} from "./get-article.handler";
 import {GetClassifyHandler} from "./get-classify.handler";
 
-export const CommandHandlers = [CreatePageHandler,CreateSitemapHandler,UpdateSitemapHandler,GetPageHandler,ClassifyCurdHandler,ArticleCurdHandler/*,GetArticleHandler*/,GetClassifyHandler];
+export const CommandHandlers = [CreatePageHandler,CreateSitemapHandler,
+    UpdateSitemapHandler,GetPageHandler,ClassifyCurdHandler,ArticleCurdHandler
+    /*,GetArticleHandler*/,GetClassifyHandler];
