9cd9aa93fe579c0b63ddc36cf12cf906d990d5a4
Eric <1945320167@qq.com>
2018-03-05 18:16:39 +0800

    Add the image configuration interface.

--- lib/modules/common/error.interface.ts
+++ lib/modules/common/error.interface.ts
@@ -5,3 +5,50 @@ export interface IErrorMessages{
     readonly errorMessage:string;
     readonly userMessage:string;
 }
+
+
+export interface ResizeData{
+    scale?:number
+    wscale?:number
+    hscale?:number
+    width?:number
+    height?:number
+    pixel?:number
+}
+export interface Resize{
+    mode:string
+    data:ResizeData
+}
+
+export interface Tailor{
+    isBefore:boolean
+    width:number
+    height:number
+    x:number
+    y:number
+    gravity:string
+}
+export interface Blur{
+    redius:number
+    sigma:number
+}
+
+export interface ImagePreProcessInfo{
+    resize?:Resize
+    tailor?:Tailor
+    watermark?:boolean
+    rotate?:number
+}
+export interface ImagePostProcessInfo{
+    resize?:Resize
+    tailor?:Tailor
+    watermark?:boolean
+    rotate?:number
+    blur?:Blur
+    sharpen?:boolean
+    format?:string
+    lossless?:boolean
+    quality?:number
+    progressive?:boolean
+    strip?:boolean
+}
