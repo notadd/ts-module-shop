7b0796b58de2dfe1d7f0dec4fda8642d2d1105ae
Eric <1945320167@qq.com>
2018-03-06 19:50:17 +0800

    Add audio configuration

---
+++ lib/modules/ext-local-store/src/interface/config/AudioFormat.ts
@@ -0,0 +1,3 @@
+export interface AudioFormat{
+    format:string
+}
---
+++ lib/modules/ext-local-store/src/interface/config/BucketConfig.ts
@@ -0,0 +1,6 @@
+export interface  BucketConfig{
+    isPublic:boolean
+    name:string
+    token_expire?:number
+    token_secret_key?:string
+}
