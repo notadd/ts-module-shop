bd1091f008a66804eab2e2ed67e06eaa1c7a6009
Eric <1945320167@qq.com>
2018-03-06 19:52:40 +0800

    Modify the page control layer return value.

---
+++ lib/modules/ext-local-store/src/model/AudioConfig.ts
@@ -0,0 +1,28 @@
+import { Entity, Column, PrimaryColumn, Index, OneToOne, JoinColumn, OneToMany } from 'typeorm';
+import { Bucket } from './Bucket'
+/* 音频配置实体类 */
+@Entity({
+    name: 'audio_config'
+})
+export class AudioConfig{
+
+  //主键，需要设置插入，1默认为公有空间配置，2默认为私有空间配置
+  @PrimaryColumn({
+    name: 'id',
+    type: 'int'
+  })
+  id: number;
+
+  //保存格式，raw、mp3、aac
+  @Column({
+    name: 'format',
+    type: 'enum',
+    enum: ['raw', 'mp3', 'aac'],
+    nullable: true
+  })
+  format: string;
+
+  @OneToOne(type => Bucket,bucket=>bucket.audio_config)
+  @JoinColumn()
+  bucket?: Bucket;
+}
