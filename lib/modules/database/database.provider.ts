4cba235d248a03668c251974b16cb5425474fc87
Eric <1945320167@qq.com>
2018-03-06 19:46:26 +0800

    Add entity class

--- lib/modules/database/database.provider.ts
+++ lib/modules/database/database.provider.ts
@@ -1,4 +1,14 @@
 import {createConnection,Connection} from "typeorm";
+import {ArticleEntity} from "../entity/article.entity";
+import {ClassifyEntity} from "../entity/classify.entity";
+import {PageEntity} from "../entity/page.entity";
+import {PageClassifyEntity} from "../entity/pageClassify.entity";
+import {PageContentEntity} from "../entity/page.content.entity";
+import {SitemapEntity} from "../entity/sitemap.entity";
+import {Bucket} from "../ext-local-store/src/model/Bucket";
+import {File} from "../ext-local-store/src/model/File";
+import {Image} from "../ext-local-store/src/model/Image";
+import {ImageConfig} from "../ext-local-store/src/model/ImageConfig";
 
 export const databaseProvider = [
     {
@@ -12,7 +22,18 @@ export const databaseProvider = [
             database: 'postgres',
             logging:false,
             entities: [
-                __dirname + '/../**/*.entity.ts',
+               /* __dirname + '/../!**!/!*.entity.ts',*/
+              /*  KindUtil,*/
+                ArticleEntity,
+                ClassifyEntity,
+                Image,
+                Bucket,
+                PageEntity,
+                PageClassifyEntity,
+                PageContentEntity,
+                SitemapEntity,
+                File,
+                ImageConfig
             ],
             synchronize: true
         }),
