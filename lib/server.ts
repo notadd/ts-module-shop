725c32da865f90f894361b62a02a41547734605d
Eric <1945320167@qq.com>
2018-03-14 14:39:16 +0800

    Add nodemon boot mode.

--- lib/server.ts
+++ lib/server.ts
@@ -1,6 +1,5 @@
 import './vendor';
 import { NestFactory } from '@nestjs/core';
-import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 import {ApplicationModule} from "./modules/app.module";
 
 /**
