6a5010fa875e1a4cb6b513e5c5cf655cb16b411e
Eric <1945320167@qq.com>
2018-03-21 12:07:25 +0800

    Upload gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,93 @@
+const fs = require('fs');
+const path = require('path');
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const gulpSequence = require('gulp-sequence');
+const sourcemaps = require('gulp-sourcemaps');
+const clean = require('gulp-clean');
+
+const packages = {
+    modules: ts.createProject('src/modules/tsconfig.json')
+};
+const modules = Object.keys(packages);
+const source = 'src';
+const distId = process.argv.indexOf('--dist');
+const dist = distId < 0 ? 'node_modules/@nestjs' : process.argv[distId + 1];
+
+gulp.task('default', function() {
+    modules.forEach(module => {
+        gulp.watch(
+            [`${source}/${module}/**/*.ts`, `${source}/${module}/*.ts`,`${source}/*.ts`],
+            [module]
+        );
+    });
+});
+
+gulp.task('copy:ts', function() {
+    return gulp.src(['src/**/*.ts']).pipe(gulp.dest('./lib'));
+});
+
+gulp.task('clean:lib', function() {
+    return gulp
+        .src(['lib/**/*.js.map', 'lib/**/*.ts', '!lib/**/*.d.ts'], { read: false })
+        .pipe(clean());
+});
+
+modules.forEach(module => {
+    gulp.task(module, () => {
+        return packages[module]
+            .src()
+            .pipe(packages[module]())
+            .pipe(gulp.dest(`${dist}/${module}`));
+    });
+});
+
+modules.forEach(module => {
+    gulp.task(module + ':dev', () => {
+    return packages[module]
+        .src()
+        .pipe(sourcemaps.init())
+        .pipe(packages[module]())
+        .pipe(
+            sourcemaps.mapSources(sourcePath => './' + sourcePath.split('/').pop())
+        )
+        .pipe(sourcemaps.write('.'))
+            .pipe(gulp.dest(`${dist}/${module}`));
+        });
+});
+
+gulp.task('build', function(cb) {
+    gulpSequence('modules', modules.filter(module => module !== 'modules'), cb);
+});
+
+gulp.task('build:dev', function(cb) {
+    gulpSequence(
+        'common:dev',
+        modules
+            .filter(module => module !== 'common')
+        .map(module => module + ':dev'),
+    'copy:ts',
+        cb
+);
+});
+
+function getFolders(dir) {
+    return fs.readdirSync(dir).filter(function(file) {
+        return fs.statSync(path.join(dir, file)).isDirectory();
+    });
+}
+gulp.task('move', function() {
+    const getDirs = (base) => getFolders(base)
+        .map((path) => `${base}/${path}`);
+
+    const examplesDirs = getDirs('examples');
+    const integrationDirs = getDirs('integration');
+    const directories = examplesDirs.concat(integrationDirs);
+
+    let stream = gulp
+        .src(['node_modules/@nestjs/**/*']);
+
+    directories.forEach((dir) => {
+        stream = stream.pipe(gulp.dest(dir + '/node_modules/@nestjs'));
+    });
+});
