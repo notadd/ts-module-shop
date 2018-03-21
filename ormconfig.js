974134a34aa498f745d11e9b7cbcc2e525c54c97
Eric <1945320167@qq.com>
2018-03-21 10:58:41 +0800

    Reduction package.json

---
+++ ormconfig.js
@@ -0,0 +1,12 @@
+const SOURCE_PATH = process.env.NODE_ENV === 'packages' ? 'dist' : 'src';
+module.exports= {
+  "type": "postgres",
+  "host": "localhost",
+  "port":  5432,
+  "username": "postgres",
+  "password": "123456",
+  "database": "postgres",
+  "entities": [`${SOURCE_PATH}/**/**.entity{.ts,.js}`],
+  "synchronize": true,
+  "logging":false
+}
