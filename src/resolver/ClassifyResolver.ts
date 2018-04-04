0b19ed596e9a59f4a81ef78b20c3dd2997f5cdbb
czy12345 <1253665625@qq.com>
2018-04-04 16:23:02 +0800

    ok

--- src/resolver/ClassifyResolver.ts
+++ src/resolver/ClassifyResolver.ts
@@ -2,6 +2,7 @@ import { ExceptionInterceptor } from '../interceptor/ExceptionInterceptor';
 import { Inject, HttpException, UseInterceptors } from '@nestjs/common';
 import { ClassifyService } from '../service/ClassifyService';
 import { Resolver, Query, Mutation } from '@nestjs/graphql';
+import { Classify } from '../interface/classify/Classify';
 import { Data } from '../interface/Data';
 import { Request } from 'express';
 
@@ -15,19 +16,29 @@ export class ClassifyResolver {
         @Inject(ClassifyService) private readonly classifyService: ClassifyService
     ) { }
 
+    @Query('classifies')
+    async classifies(req: Request, body: { parentId: number, level: 1 | 2 | 3 }): Promise<Data & { classifes: Classify[] }> {
+        let { parentId, level } = body
+        if (level !== 1 && level !== 2 && level !== 3) {
+            throw new HttpException('参数错误', 400)
+        }
+        let classifes: Classify[] = await this.classifyService.getClassifes(parentId, level)
+        return { code: 200, message: '获取指定分类成功', classifes }
+    }
+
     @Mutation('createClassify')
-    async createClassify(req: Request, body: { name: string, description: string, level: 1 | 2 | 3 ,parentId:number}): Promise<Data> {
-        let { name, description, level,parentId } = body
+    async createClassify(req: Request, body: { name: string, description: string, level: 1 | 2 | 3, parentId: number }): Promise<Data> {
+        let { name, description, level, parentId } = body
         if (!name || !description || !level) {
             throw new HttpException('缺少参数', 400)
         }
         if (level !== 1 && level !== 2 && level !== 3) {
             throw new HttpException('参数错误', 400)
         }
-        if(level!==1&&!parentId){
+        if (level !== 1 && !parentId) {
             throw new HttpException('缺少上级分类', 400)
         }
-        await this.classifyService.createClassify(name, description, level,parentId)
+        await this.classifyService.createClassify(name, description, level, parentId)
         return { code: 200, message: '创建分类成功' }
     }
 }
