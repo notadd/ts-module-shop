41ee987da808bb900ff66d7fed530e4fb15807ec
czy12345 <1253665625@qq.com>
2018-04-04 14:20:04 +0800

    FirstClassify

---
+++ src/model/FirstClassify.entity.ts
@@ -0,0 +1,26 @@
+import { Entity,PrimaryGeneratedColumn,Column,CreateDateColumn,UpdateDateColumn,OneToMany } from 'typeorm';
+
+@Entity('first_classify')
+export class FirstClassify{
+
+    @PrimaryGeneratedColumn({type:'integer',name:'id'})
+    id:number
+
+    @Column({
+        name:'name',
+        type:'varchar',
+        length:20,
+        unique:true
+    })
+    name:string
+
+    @Column({
+        name:'description',
+        type:'varchar',
+        length:100
+    })
+    description:string
+
+
+
+}
