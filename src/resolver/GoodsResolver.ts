df221cd2814d6578ace095bebd6f6e2b3bdcbaa2
czy12345 <1253665625@qq.com>
2018-04-08 10:26:20 +0800

    createGoods

--- src/resolver/GoodsResolver.ts
+++ src/resolver/GoodsResolver.ts
@@ -1,15 +1,25 @@
 import { ExceptionInterceptor } from '../interceptor/ExceptionInterceptor';
-import { Inject,HttpException,UseInterceptors } from '@nestjs/common';
-import { Resolver,Mutation,Query } from '@nestjs/graphql'
+import { Inject, HttpException, UseInterceptors } from '@nestjs/common';
+import { Resolver, Mutation, Query } from '@nestjs/graphql';
+import { GoodsService } from '../service/GoodsService';
 import { Data } from '../interface/Data';
 
-@Resolver('Goods')
-export class GoodsResolver{
 
-    constructor(){}
+@Resolver('Goods')
+@UseInterceptors(ExceptionInterceptor)
+export class GoodsResolver {
+
+    constructor(
+        @Inject(GoodsService) private readonly goodsService: GoodsService
+    ) { }
 
     @Mutation('createGoods')
-    async createGoods(req:Request,body:{}):Promise<Data>{
-        
+    async createGoods(req: Request, body: { name: string, description: string }): Promise<Data> {
+        let { name, description } = body;
+        if (!name || !description) {
+            throw new HttpException('缺少参数', 400)
+        }
+        await this.goodsService.createGoods(name, description)
+        return { code: 200, message: '创建商品成功' }
     }
 }
---
+++ src/service/GoodsService.ts
@@ -0,0 +1,11 @@
+import { Component, HttpException, Inject } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Component()
+export class GoodsService {
+
+    constructor() { }
+
+
+}
