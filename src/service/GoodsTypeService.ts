67f2201015bbbbd16e65e1670b7d3670d3f7cf89
czy12345 <1253665625@qq.com>
2018-04-09 10:30:48 +0800

    add createGoodsType service method

--- src/service/GoodsTypeService.ts
+++ src/service/GoodsTypeService.ts
@@ -11,4 +11,16 @@ export class GoodsTypeService {
         @InjectRepository(GoodsType) private readonly goodsTypeRepository: Repository<GoodsType>,
         @InjectRepository(GoodsProperty) private readonly goodsPropertyRepository: Repository<GoodsProperty>
     ) { }
+
+    async createGoodsType(name: string): Promise<void> {
+        let exist: GoodsType = await this.goodsTypeRepository.findOne({ name })
+        if (exist) {
+            throw new HttpException('指定name=' + name + '商品类型已存在', 404)
+        }
+        try {
+            await this.goodsTypeRepository.save({ name })
+        } catch (err) {
+            throw new HttpException('发生了数据库错误' + err.toString(), 403)
+        }
+    }
 }
