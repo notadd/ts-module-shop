2761ab12aae4c6d5702a2e9de73e9e6a31138ff9
czy12345 <1253665625@qq.com>
2018-04-09 13:52:05 +0800

    add goodsTypes resolver

--- src/resolver/GoodsTypeResolver.ts
+++ src/resolver/GoodsTypeResolver.ts
@@ -13,6 +13,12 @@ export class GoodsTypeResolver {
         @Inject(GoodsTypeService) private readonly goodsTypeService: GoodsTypeService
     ) { }
 
+    @Query('goodsTypes')
+    async goodsTypes(req: Request): Promise<Data & { goodsTypes: { id: number, name: string }[] }> {
+        let goodsTypes: { id: number, name: string }[] = await this.goodsTypeService.getGoodsTypes()
+        return { code: 200, message: '获取所有商品类型成功', goodsTypes }
+    }
+
     @Mutation('createGoodsType')
     async createGoodsType(req: Request, body: { name: string }): Promise<Data> {
         let { name } = body
@@ -24,23 +30,23 @@ export class GoodsTypeResolver {
     }
 
     @Mutation('updateGoodsType')
-    async updateGoodsType(req:Request,body:{id:number,name:string}):Promise<Data>{
-        let {id,name} = body
-        if(!id){
-            throw new HttpException('缺少参数',404)
+    async updateGoodsType(req: Request, body: { id: number, name: string }): Promise<Data> {
+        let { id, name } = body
+        if (!id) {
+            throw new HttpException('缺少参数', 404)
         }
-        await this.goodsTypeService.updateGoodsType(id,name)
-        return {code:200,message:'更新商品类型成功'}
+        await this.goodsTypeService.updateGoodsType(id, name)
+        return { code: 200, message: '更新商品类型成功' }
     }
 
     @Mutation('deleteGoodsType')
-    async deleteGoodsType(req:Request,body:{id:number}):Promise<Data>{
-        let {id} = body
-        if(!id){
-            throw new HttpException('缺少参数',404)
+    async deleteGoodsType(req: Request, body: { id: number }): Promise<Data> {
+        let { id } = body
+        if (!id) {
+            throw new HttpException('缺少参数', 404)
         }
         await this.goodsTypeService.deleteGoodsType(id)
-        return {code:200,message:'删除商品类型成功'}
+        return { code: 200, message: '删除商品类型成功' }
     }
 
 
