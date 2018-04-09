efd014b001714553cfdac6b1d081514626b82e3a
czy12345 <1253665625@qq.com>
2018-04-09 11:44:21 +0800

    add createGoodsProperty resolver

--- src/resolver/GoodsPropertyResolver.ts
+++ src/resolver/GoodsPropertyResolver.ts
@@ -6,10 +6,30 @@ import { Data } from '../interface/Data';
 import { Request } from 'express';
 
 @Resolver('GoodsProperty')
+@UseInterceptors(ExceptionInterceptor)
 export class GoodsPropertyResolver {
 
     constructor(
-        @Inject(GoodsPropertyService) private readonly goodsPropertyService:GoodsPropertyService
+        @Inject(GoodsPropertyService) private readonly goodsPropertyService: GoodsPropertyService
     ) { }
+
+    @Mutation('createGoodsProperty')
+    async createGoodsProperty(req: Request, body: { name: string, type: 'unique' | 'radio' | 'check', inputType: 'text' | 'list' | 'textarea', list: string[] }): Promise<Data> {
+        let { name, type, inputType, list } = body
+        if (!name || !type || !inputType) {
+            throw new HttpException('缺少参数', 404)
+        }
+        if (type !== 'unique' && type !== 'radio' && type !== 'check') {
+            throw new HttpException('type参数不正确', 404)
+        }
+        if (inputType !== 'text' && inputType !== 'list' && inputType !== 'textarea') {
+            throw new HttpException('inputType参数不正确', 404)
+        }
+        if (inputType === 'list' && !list) {
+            throw new HttpException('输入类型为list时，list列表值必须存在', 404)
+        }
+        await this.goodsPropertyService.createGoodsProperty(name, type, inputType, list)
+        return { code: 200, message: '创建商品属性成功' }
+    }
 
 }
