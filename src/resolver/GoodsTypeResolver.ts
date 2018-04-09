a730967b4d58e8193865f33e518ff78cadc31417
czy12345 <1253665625@qq.com>
2018-04-09 10:26:57 +0800

    add createGoodsType resolver

--- src/resolver/GoodsTypeResolver.ts
+++ src/resolver/GoodsTypeResolver.ts
@@ -1,11 +1,28 @@
-import { Resolver,Query,Mutation } from '@nestjs/graphql'
-import { Inject,HttpException } from '@nestjs/common';
+import { ExceptionInterceptor } from '../interceptor/ExceptionInterceptor';
+import { Inject, HttpException, UseInterceptors } from '@nestjs/common';
+import { GoodsTypeService } from '../service/GoodsTypeService';
+import { Resolver, Query, Mutation } from '@nestjs/graphql'
+import { Data } from '../interface/Data';
+import { Request } from 'express';
 
 @Resolver('GoodsTyoe')
-export class GoodsTypeResolver{
+@UseInterceptors(ExceptionInterceptor)
+export class GoodsTypeResolver {
 
     constructor(
-        @Inject() 
-    ){}
+        @Inject(GoodsTypeService) private readonly goodsTypeService: GoodsTypeService
+    ) { }
+
+    @Mutation('createGoodsType')
+    async createGoodsType(req: Request, body: { name: string }): Promise<Data> {
+        let { name } = body
+        if (!name) {
+            throw new HttpException('缺少参数', 404)
+        }
+        await this.goodsTypeService.createGoodsType(name)
+        return { code: 200, message: '创建商品类型成功' }
+    }
+
+
 
 }
