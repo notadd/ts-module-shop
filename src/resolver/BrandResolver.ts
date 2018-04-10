35466259eb30458170cceac1fe471100e9a0f2ea
czy12345 <1253665625@qq.com>
2018-04-10 12:00:22 +0800

    add updateBrand resolver

--- src/resolver/BrandResolver.ts
+++ src/resolver/BrandResolver.ts
@@ -17,12 +17,22 @@ export class BrandResolver {
 
     @Mutation('createBrand')
     async createBrand(req: Request, body: { name: string }): Promise<Data> {
-        let {name} = body
-        if(!name){
-            throw new HttpException('缺少参数',404)
+        let { name } = body
+        if (!name) {
+            throw new HttpException('缺少参数', 404)
         }
         await this.brandService.createBrand(name)
-        return {code:200,message:'创建品牌成功'}
+        return { code: 200, message: '创建品牌成功' }
+    }
+
+    @Mutation('updateBrand')
+    async updateBrand(req: Request, body: { id: number, name: string }): Promise<Data> {
+        let { id,name } = body
+        if (!id||!name) {
+            throw new HttpException('缺少参数', 404)
+        }
+        await this.brandService.updateBrand(id,name)
+        return { code: 200, message: '更新品牌成功' }
     }
 
 }
