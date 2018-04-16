87acc57ede2c1168f90e51f8a36b2eba05c0b5eb
czy12345 <1253665625@qq.com>
2018-04-16 10:19:05 +0800

    add createGoodsImage resolver

--- src/resolver/goods.image.resolver.ts
+++ src/resolver/goods.image.resolver.ts
@@ -18,12 +18,22 @@ export class GoodsImageResolver {
 
     @Query("goodsImages")
     async goodsImages(req: Request, body: { goodsId: number }): Promise<GoodsImageData> {
-        const {goodsId} = body;
+        const { goodsId } = body;
         if (!goodsId) {
             throw new HttpException("指定id=" + goodsId + "商品不存在", 404);
         }
-        const images: Array<{id: number; bucketName: string; name: string, type: string; url: string}> = await this.goodsImageService.getGoodsImages(req, goodsId);
-        return {code: 200, message: "获取指定商品的所有图片成功", images};
+        const images: Array<{ id: number; bucketName: string; name: string, type: string; url: string }> = await this.goodsImageService.getGoodsImages(req, goodsId);
+        return { code: 200, message: "获取指定商品的所有图片成功", images };
+    }
+
+    @Mutation("createGoodsImage")
+    async createGoodsImage(req: Request, body: { goodsId: number, bucketName: string, rawName: string, base64: string }): Promise<Data> {
+        const { goodsId, bucketName, rawName, base64 } = body;
+        if (!goodsId || !bucketName || !rawName || !base64) {
+            throw new HttpException("缺少参数", 404);
+        }
+        await this.goodsImages.createGoodsImage(goodsId, bucketName, rawName, base64);
+        return { code: 200, message: "创建商品图片成功" };
     }
 
 }
