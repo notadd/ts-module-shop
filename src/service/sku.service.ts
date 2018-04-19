5e8af99faa963223a09ea4fba0a07f9dc3c00aab
czy12345 <1253665625@qq.com>
2018-04-19 14:30:52 +0800

    add createSku method to SkuService

--- src/service/sku.service.ts
+++ src/service/sku.service.ts
@@ -12,8 +12,32 @@ export class SkuService {
     constructor(
         @InjectRepository(Sku) private readonly skuRepository: Repository<Sku>,
         @InjectRepository(Goods) private readonly goodsRepository: Repository<Goods>,
-        @InjectRepository(PropertyValue) private readonly propertyVValueRepository: Repository<PropertyValue>
+        @InjectRepository(PropertyValue) private readonly propertyValueRepository: Repository<PropertyValue>
     ) { }
 
+    async createSku(goodsId: number, inventory: number, propertyValueIds: Array<number>): Promise<void> {
+        const goods: Goods | undefined = await this.goodsRepository.findOneById(goodsId);
+        if (!goods) {
+            throw new HttpException("指定id=" + goodsId + "商品不存在", 404);
+        }
+        const values: Array<PropertyValue> | undefined = await this.propertyValueRepository.findByIds(propertyValueIds, { relations: ["property", "goods"] });
+        for (let i = 0; i < propertyValueIds.length; i++) {
+            const find: PropertyValue | undefined = values.find(value => value.id === propertyValueIds[i]);
+            if (!find) {
+                throw new HttpException("指定id=" + propertyValueIds[i] + "属性值不存在", 404);
+            }
+            if (find.goods.id !== goods.id) {
+                throw new HttpException("指定id=" + propertyValueIds[i] + "属性值不存在于指定id=" + goods.id + "商品之下", 404);
+            }
+            if (find.property.type !== "radio") {
+                throw new HttpException("指定id=" + propertyValueIds[i] + "属性值的属性不是单选类型，只有单选类型属性值可以作为sku添加", 404);
+            }
+            try {
+                await this.skuRepository.save({ inventory, goods, values });
+            } catch (err) {
+                throw new HttpException("发生了数据库错误" + err.toString(), 403);
+            }
+        }
 
+    }
 }
