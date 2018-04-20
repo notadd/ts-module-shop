c3c9865a094e03d8c8eade40c130b68fd4ae7ca2
czy12345 <1253665625@qq.com>
2018-04-20 10:51:13 +0800

    add createDelivery method to resolver

--- src/resolver/delivery.resolver.ts
+++ src/resolver/delivery.resolver.ts
@@ -19,7 +19,17 @@ export class DeliveryResolver {
     @Query("deliveries")
     async deliveries(req: Request): Promise<DeliveriesData> {
         const deliveries = await this.deliveryService.getDeliveries();
-        return {code: 200, message: "获取所有配送服务成功", deliveries};
+        return { code: 200, message: "获取所有配送服务成功", deliveries };
+    }
+
+    @Mutation("createDelivery")
+    async createDelivery(req: Request, body: { name: string, description: string, cost: number, freeLimit: number, valuationFee: number }): Promise<Data> {
+        const { name, description, cost, freeLimit, valuationFee} = body;
+        if (!name || !description || cost === undefined || cost === null || freeLimit === undefined || freeLimit === null || valuationFee === undefined || valuationFee === null) {
+            throw new HttpException("缺少参数", 404);
+        }
+        await this.deliveryService.createDelivery(name, description, cost, freeLimit, valuationFee);
+        return {code: 200, message: "创建配送信息成功"};
     }
 
 }
