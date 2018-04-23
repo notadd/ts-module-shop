056f780df23765d89f38b5c57028d6e75b557f22
czy12345 <1253665625@qq.com>
2018-04-23 10:25:59 +0800

    add createUserReceivingInformation method to service

--- src/service/user.receiving.information.service.ts
+++ src/service/user.receiving.information.service.ts
@@ -1,6 +1,7 @@
 import { UserReceivingInformation } from "../model/user.receiving.information.entity";
 import { Component, HttpException, Inject } from "@nestjs/common";
 import { PropertyValue } from "../model/property.value.entity";
+import { UserComponent } from "../interface/user.component";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 
@@ -9,9 +10,29 @@ import { Repository } from "typeorm";
 export class UserReceivingInformationService {
 
     constructor(
-        @Inject("UserComponentToken") private readonly userComponent: any,
+        @Inject("UserComponentToken") private readonly userComponent: UserComponent,
         @InjectRepository(UserReceivingInformation) private readonly userReceivingInformationRepository: Repository<UserReceivingInformation>
     ) { }
 
+    async createUserReceivingInformation(
+        userId: number,
+        consignee: string,
+        email: string,
+        region: string,
+        address: string,
+        postCode: string,
+        homePhone: string,
+        mobilePhone: string
+    ): Promise<void> {
+        const user: { id: number, userName: string, status: boolean, recycle: boolean }; = await this.userComponent.getUserById(userId);
+        if (!user) {
+            throw new HttpException("指定id=" + userId + "用户不存在", 404);
+        }
+        try {
+            await this.userReceivingInformationRepository.save({ userId, consignee, email, region, address, postCode, homePhone, mobilePhone });
+        } catch (err) {
+            throw new HttpException("发生了数据库错误" + err.toString(), 403);
+        }
+    }
 
 }
