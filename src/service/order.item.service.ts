d7b90141c378e991a2b2240d934334e3db316de4
czy12345 <1253665625@qq.com>
2018-04-25 10:21:16 +0800

    add createOrderItem graphql type

--- src/service/order.item.service.ts
+++ src/service/order.item.service.ts
@@ -2,13 +2,13 @@ import { Repository, Connection, QueryRunner, SelectQueryBuilder } from "typeorm
 import { Component, HttpException, Inject } from "@nestjs/common";
 import { OrderItem } from "../model/order.item.entity";
 import { InjectRepository } from "@nestjs/typeorm";
-
-
+import { Sku } from "../model/sku.entity";
 /* 订单项服务组件 */
 @Component()
 export class OrderItemService {
 
     constructor(
+        @InjectRepository(Sku) private readonly skuRepository: Repository<sKU>,
         @InjectRepository(OrderItem) private readonly orderItemRepository: Repository<OrderItem>,
     ) { }
 
