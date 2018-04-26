359c55b9bd06c172b0b0dc1de36f447e38862cea
czy12345 <1253665625@qq.com>
2018-04-26 10:44:26 +0800

    add createOrderFromCart

--- src/service/order.service.ts
+++ src/service/order.service.ts
@@ -2,6 +2,7 @@ import { UserReceivingInformation } from "../model/user.receiving.information.en
 import { Repository, Connection, QueryRunner, SelectQueryBuilder } from "typeorm";
 import { Component, HttpException, Inject } from "@nestjs/common";
 import { UserComponent } from "../interface/user.component";
+import { OrderItem } from "../model/order.item.entity";
 import { Delivery } from "../model/delivery.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 import { RandomUtil } from "../util/random.util";
@@ -18,6 +19,7 @@ export class OrderService {
         @Inject(RandomUtil) private readonly randomUtil: RandomUtil,
         @Inject("UserComponentToken") private readonly userComponent: UserComponent,
         @InjectRepository(Order) private readonly orderRepository: Repository<Order>,
+        @InjectRepository(OrderItem) private readonly orderItemRepository: Repository<OrderItem>,
         @InjectRepository(Delivery) private readonly deliveryRepository: Repository<Delivery>,
         @InjectRepository(UserReceivingInformation) private readonly userReceivingInformationRepository: Repository<UserReceivingInformation>
     ) { }
@@ -66,6 +68,52 @@ export class OrderService {
         }
         /* 生成32位订单号  */
         const orderNo = this.dateUtil.getString(new Date()) + this.randomUtil.getRandom(18);
+        try {
+            await this.orderRepository.save({ orderNo, userId, delivertNo, delivertTime: new Date(delivertTime), invoiceType, invoiceContent, invoiceTitle, customerMessage, delivery, userReceivingInformation, items });
+        } catch (err) {
+            throw new HttpException("发生了数据库错误" + err.toString(), 403);
+        }
+    }
+
+    async createOrderFromCart(
+        userId: number,
+        delivertNo: string,
+        delivertTime: string,
+        invoiceType: string,
+        invoiceContent: string,
+        invoiceTitle: string,
+        customerMessage: string,
+        deliveryId: number,
+        userReceivingInformationId: number,
+        itemIds: Array<number>
+    ): Promise<void> {
+        const user: { id: number, userName: string, status: boolean, recycle: boolean } = await this.userComponent.getUserById(userId);
+        if (!user) {
+            throw new HttpException("指定id=" + userId + "用户不存在", 404);
+        }
+        const delivery: Delivery | undefined = await this.deliveryRepository.findOneById(deliveryId);
+        if (!delivery) {
+            throw new HttpException("指定id=" + deliveryId + "配送信息不存在", 404);
+        }
+        const userReceivingInformation: UserReceivingInformation | undefined = await this.userReceivingInformationRepository.findOneById(userReceivingInformationId);
+        if (!userReceivingInformation) {
+            throw new HttpException("缺少参数", 404);
+        }
+        /* 生成32位订单号  */
+        const orderNo = this.dateUtil.getString(new Date()) + this.randomUtil.getRandom(18);
+        const items: Array<OrderItem> | undefined = await this.orderItemRepository.findByIds(itemIds, { relations: ["order"] });
+        itemIds.forEach(id => {
+            const item: OrderItem | undefined = items.find(i => i.id === id);
+            if (!item) {
+                throw new HttpException("指定id=" + id + "订单项不存在", 404);
+            }
+            if (item.userId !== userId) {
+                throw new HttpException("指定订单项不属于指定id=" + userId + "用户", 404);
+            }
+            if (item.order) {
+                throw new HttpException("指定订单项id=" + id + "已属于订单", 404);
+            }
+        });
         try {
             await this.orderRepository.save({ orderNo, userId, delivertNo, delivertTime: new Date(delivertTime), invoiceType, invoiceContent, invoiceTitle, customerMessage, delivery, userReceivingInformation, items });
         } catch (err) {
