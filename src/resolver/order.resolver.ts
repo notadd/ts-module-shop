4d38d34fd173def03f01a49a9f2c2a0708e112d8
czy12345 <1253665625@qq.com>
2018-04-26 10:37:10 +0800

    add createOrderFromCart resolver method

--- src/resolver/order.resolver.ts
+++ src/resolver/order.resolver.ts
@@ -54,6 +54,27 @@ export class OrderResolver {
         return { code: 200, message: "创建订单成功" };
     }
 
+    @Mutation("createOrderFromCart")
+    async createOrderFromCart(req: Request, body: {
+        userId: number,
+        delivertNo: string,
+        delivertTime: string,
+        invoiceType: string,
+        invoiceContent: string,
+        invoiceTitle: string,
+        customerMessage: string,
+        deliveryId: number,
+        userReceivingInformationId: number,
+        itemIds: Array<number>
+    }): Promise<Data> {
+        const { userId, delivertNo, delivertTime, invoiceType, invoiceContent, invoiceTitle, customerMessage, deliveryId, userReceivingInformationId, itemIds } = body;
+        if (!userId || !delivertNo || !delivertTime || !invoiceType || !invoiceContent || !invoiceTitle || !customerMessage || !deliveryId || !userReceivingInformationId || !itemIds || itemIds.length === 0) {
+            throw new HttpException("缺少参数", 404);
+        }
+        await this.orderService.createOrderFromCart(userId, delivertNo, delivertTime, invoiceType, invoiceContent, invoiceTitle, customerMessage, deliveryId, userReceivingInformationId, itemIds);
+        return { code: 200, message: "从购物车创建订单成功" };
+    }
+
     @Mutation("updateOrder")
     async updateOrder(req: Request, body: {
         id: number,
