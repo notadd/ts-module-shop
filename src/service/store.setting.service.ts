a3f4f2828206cba05515776e0ebce47f895296a5
czy12345 <1253665625@qq.com>
2018-04-28 10:50:44 +0800

    saveStoreSetting method to service

--- src/service/store.setting.service.ts
+++ src/service/store.setting.service.ts
@@ -1,4 +1,5 @@
 import { Component, HttpException, Inject } from "@nestjs/common";
+import { StoreComponent } from "../interface/store.component";
 import { StoreSetting } from "../model/store.setting.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
@@ -8,7 +9,39 @@ import { Repository } from "typeorm";
 export class StoreSettingService {
 
     constructor(
+        @Inject("StoreComponentToken") private readonly storeComponent: StoreComponent,
         @InjectRepository(StoreSetting) private readonly storeSettingRepository: Repository<StoreSetting>
     ) { }
+
+    async saveStoreSetting(
+        logoBucketName: string,
+        logoRawName: string,
+        logoBase64: string,
+        title: string,
+        region: string,
+        address: string,
+        close: string,
+        closeReason: string,
+        servicePhone: string
+    ): Promise<void> {
+        const { bucketName, name, type } = await this.storeComponent.upload(logoBucketName, logoRawName, logoBase64, undefined);
+        try {
+            const setting: StoreSetting = this.storeSettingRepository.create({
+                id: 1,
+                logoBucketName: bucketName,
+                logoName: name,
+                logoType: type,
+                title,
+                region,
+                address,
+                close: !!close,
+                closeReason,
+                servicePhone
+            });
+            await this.storeSettingRepository.save(setting);
+        } catch (err) {
+            throw new HttpException("发生了数据库错误" + err.toString(), 403);
+        }
+    }
 
 }
