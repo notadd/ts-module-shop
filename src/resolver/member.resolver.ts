418dcb491dc36b3d4fbb1f2e77e2b19e25c7e930
czy12345 <1253665625@qq.com>
2018-05-02 10:29:46 +0800

    add createMember method to resolver

--- src/resolver/member.resolver.ts
+++ src/resolver/member.resolver.ts
@@ -15,4 +15,23 @@ export class MemberResolver {
         @Inject(MemberService) private readonly memberService: MemberService
     ) { }
 
+    @Mutation("createMember")
+    async createMember(req: Request, body: {
+        name: string,
+        realName: string,
+        email: string,
+        sex: string,
+        idNumber: string,
+        birthday: string,
+        password: string
+        mobilePhone: string
+    }): Promise<Data> {
+        const { name, realName, email, sex, idNumber, birthday, password, mobilePhone } = body;
+        if (!name || !realName || !email || !sex || !idNumber || !birthday || !password || !mobilePhone) {
+            throw new HttpException("缺少参数", 404);
+        }
+        await this.memberService.createMember(name, realName, email, sex, idNumber, birthday, password, mobilePhone);
+        return { code: 200, message: "创建会员成功" };
+    }
+
 }
