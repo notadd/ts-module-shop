ff1b0fad461d4bba6cccd5614768252ae5266d14
czy12345 <1253665625@qq.com>
2018-05-02 10:14:47 +0800

    add member entity class

---
+++ src/model/member.entity.ts
@@ -0,0 +1,35 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, OneToOne, OneToMany, ManyToOne } from "typeorm";
+
+
+/* 会员实体类 */
+@Entity("member")
+export class Member {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ length: 20 })
+    name: string;
+
+    @Column({ length: 20 })
+    realName: string;
+
+    @Column({ length: 20 })
+    email: string;
+
+    @Column({ length: 10 })
+    sex: string;
+
+    @Column({ length: 20 })
+    idNumber: string;
+
+    @Column()
+    birthday: Date;
+
+    @Column({ length: 20 })
+    password: string;
+
+    @Column({ length: 20 })
+    mobilePhone: string;
+
+}
