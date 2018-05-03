2bfb6c543ec25d7fe7701088f52db0541b539acb
czy12345 <1253665625@qq.com>
2018-05-03 10:34:01 +0800

    complete createFloor method to service

--- src/service/floor.service.ts
+++ src/service/floor.service.ts
@@ -15,12 +15,22 @@ export class FloorService {
     ) { }
 
     async createFloor(name: string, display: string, goodsIds: Array<number>): Promise<void> {
-        const exist: Floor|undefined = await this.floorRepository.findOne({name});
-        if(exist) {
+        const exist: Floor | undefined = await this.floorRepository.findOne({ name });
+        if (exist) {
             throw new HttpException("指定name=" + name + "楼层已存在", 404);
         }
-        const goodses:Array<Goods>|undefined = await this.goodsRepository.findByIds(goodsIds);
-        
+        const goodses: Array<Goods> | undefined = await this.goodsRepository.findByIds(goodsIds);
+        for (let i = 0; i < goodsIds.length; i++) {
+            const find: Goods | undefined = goodses.find(goods => goods.id === goodsIds[i]);
+            if (!find) {
+                throw new HttpException("指定id=" + goodsIds[i] + "商品未找到", 404);
+            }
+        }
+        try {
+            await this.floorRepository.save({ name, display: !!display, goodses });
+        } catch (err) {
+            throw new HttpException("发生了数据库错误" + err.toString(), 403);
+        }
     }
 
 }
