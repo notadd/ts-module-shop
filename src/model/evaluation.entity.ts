1619afca0d76d2d5f6c0ae1480b58d7cb77fe36f
czy12345 <1253665625@qq.com>
2018-05-08 13:58:08 +0800

    add Evlauation entity class

---
+++ src/model/evaluation.entity.ts
@@ -0,0 +1,14 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, OneToOne, OneToMany, ManyToOne } from "typeorm";
+
+@Entity("evaluation")
+export class Evaluation{
+
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column({length:200})
+    content:string;
+
+
+
+}
