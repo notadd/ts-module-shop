fadc7ea1241c3de9cf4e17f35b7a1c316c56e398
czy12345 <1253665625@qq.com>
2018-05-09 16:09:33 +0800

    deleteEvaluation method to resolver

--- src/resolver/evaluation.resolver.ts
+++ src/resolver/evaluation.resolver.ts
@@ -34,4 +34,14 @@ export class EvaluationResolver {
         return { code: 200, message: "更新评价成功" };
     }
 
+    @Mutation("deleteEvaluation")
+    async deleteEvaluation(req: Request, body: { id: number }): Promise<Data> {
+        const { id } = body;
+        if (!id) {
+            throw new HttpException("缺少参数", 400);
+        }
+        await this.evaluationService.deleteEvaluation(id);
+        return { code: 200, message: "删除评价成功" };
+    }
+
 }
